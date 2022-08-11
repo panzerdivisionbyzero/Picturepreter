661f4d2b0eee8390bf4b7a6edd6e235e90dd8b33
panzerdivisionbyzero <pawel.vitek.witkowski@gmail.com>
2022-08-11 14:37:27 +0200

    added basic components and loading bitmap feature;

--- BitmapsPxDiff/FrmMain.Designer.cs
+++ BitmapsPxDiff/FrmMain.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLoadBitmap = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.od = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 320F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnLoadBitmap, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pb, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnLoadBitmap
+            // 
+            this.btnLoadBitmap.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoadBitmap.Location = new System.Drawing.Point(16, 16);
+            this.btnLoadBitmap.Margin = new System.Windows.Forms.Padding(16);
+            this.btnLoadBitmap.Name = "btnLoadBitmap";
+            this.btnLoadBitmap.Size = new System.Drawing.Size(288, 23);
+            this.btnLoadBitmap.TabIndex = 0;
+            this.btnLoadBitmap.Text = "Load bitmap";
+            this.btnLoadBitmap.UseVisualStyleBackColor = true;
+            this.btnLoadBitmap.Click += new System.EventHandler(this.btnLoadBitmap_Click);
+            // 
+            // pb
+            // 
+            this.pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb.Location = new System.Drawing.Point(336, 16);
+            this.pb.Margin = new System.Windows.Forms.Padding(16);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(448, 418);
+            this.pb.TabIndex = 1;
+            this.pb.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btnLoadBitmap;
+        private PictureBox pb;
+        private OpenFileDialog od;
     }
 }
--- BitmapsPxDiff/FrmMain.cs
+++ BitmapsPxDiff/FrmMain.cs
@@ -2,9 +2,19 @@ namespace BitmapsPxDiff
 {
     public partial class FrmMain : Form
     {
+        private Bitmap bmp;
         public FrmMain()
         {
             InitializeComponent();
         }
+
+        private void btnLoadBitmap_Click(object sender, EventArgs e)
+        {
+            if (od.ShowDialog() != DialogResult.OK) return;
+            this.btnLoadBitmap.Text = Path.GetFileName(od.FileName);
+            bmp = new Bitmap(od.FileName);
+            pb.Image = bmp;
+        }
+
     }
 }
