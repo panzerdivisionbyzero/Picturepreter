c44c3df7ce2318106b089d4ff6668b5c42e69ced
panzerdivisionbyzero <pawel.vitek.witkowski@gmail.com>
2022-08-11 22:53:21 +0200

    added loading two bitmaps feature;

--- BitmapsPxDiff/FrmMain.Designer.cs
+++ BitmapsPxDiff/FrmMain.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnLoadBitmap = new System.Windows.Forms.Button();
             this.pb = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLoadImage2 = new System.Windows.Forms.Button();
+            this.btnLoadImage1 = new System.Windows.Forms.Button();
+            this.gbPreviewMode = new System.Windows.Forms.GroupBox();
+            this.tbPreviewModeResult = new System.Windows.Forms.RadioButton();
+            this.rbPreviewModeImg2 = new System.Windows.Forms.RadioButton();
+            this.rbPreviewModeImg1 = new System.Windows.Forms.RadioButton();
             this.od = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.gbPreviewMode.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -41,8 +49,8 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 320F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.btnLoadBitmap, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.pb, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -50,18 +58,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // btnLoadBitmap
-            // 
-            this.btnLoadBitmap.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnLoadBitmap.Location = new System.Drawing.Point(16, 16);
-            this.btnLoadBitmap.Margin = new System.Windows.Forms.Padding(16);
-            this.btnLoadBitmap.Name = "btnLoadBitmap";
-            this.btnLoadBitmap.Size = new System.Drawing.Size(288, 23);
-            this.btnLoadBitmap.TabIndex = 0;
-            this.btnLoadBitmap.Text = "Load bitmap";
-            this.btnLoadBitmap.UseVisualStyleBackColor = true;
-            this.btnLoadBitmap.Click += new System.EventHandler(this.btnLoadBitmap_Click);
             // 
             // pb
             // 
@@ -73,6 +69,100 @@
             this.pb.TabIndex = 1;
             this.pb.TabStop = false;
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnLoadImage2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnLoadImage1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.gbPreviewMode, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(314, 444);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btnLoadImage2
+            // 
+            this.btnLoadImage2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoadImage2.Location = new System.Drawing.Point(16, 71);
+            this.btnLoadImage2.Margin = new System.Windows.Forms.Padding(16);
+            this.btnLoadImage2.Name = "btnLoadImage2";
+            this.btnLoadImage2.Size = new System.Drawing.Size(282, 23);
+            this.btnLoadImage2.TabIndex = 1;
+            this.btnLoadImage2.Text = "Load image 2";
+            this.btnLoadImage2.UseVisualStyleBackColor = true;
+            this.btnLoadImage2.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnLoadImage1
+            // 
+            this.btnLoadImage1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoadImage1.Location = new System.Drawing.Point(16, 16);
+            this.btnLoadImage1.Margin = new System.Windows.Forms.Padding(16);
+            this.btnLoadImage1.Name = "btnLoadImage1";
+            this.btnLoadImage1.Size = new System.Drawing.Size(282, 23);
+            this.btnLoadImage1.TabIndex = 0;
+            this.btnLoadImage1.Text = "Load image 1";
+            this.btnLoadImage1.UseVisualStyleBackColor = true;
+            this.btnLoadImage1.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // gbPreviewMode
+            // 
+            this.gbPreviewMode.Controls.Add(this.tbPreviewModeResult);
+            this.gbPreviewMode.Controls.Add(this.rbPreviewModeImg2);
+            this.gbPreviewMode.Controls.Add(this.rbPreviewModeImg1);
+            this.gbPreviewMode.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gbPreviewMode.Location = new System.Drawing.Point(3, 113);
+            this.gbPreviewMode.Name = "gbPreviewMode";
+            this.gbPreviewMode.Size = new System.Drawing.Size(308, 105);
+            this.gbPreviewMode.TabIndex = 3;
+            this.gbPreviewMode.TabStop = false;
+            this.gbPreviewMode.Text = "Preview mode";
+            // 
+            // tbPreviewModeResult
+            // 
+            this.tbPreviewModeResult.AutoSize = true;
+            this.tbPreviewModeResult.Location = new System.Drawing.Point(13, 72);
+            this.tbPreviewModeResult.Name = "tbPreviewModeResult";
+            this.tbPreviewModeResult.Size = new System.Drawing.Size(93, 19);
+            this.tbPreviewModeResult.TabIndex = 2;
+            this.tbPreviewModeResult.TabStop = true;
+            this.tbPreviewModeResult.Text = "Result image";
+            this.tbPreviewModeResult.UseVisualStyleBackColor = true;
+            this.tbPreviewModeResult.CheckedChanged += new System.EventHandler(this.rbPreviewModeImg_CheckedChanged);
+            // 
+            // rbPreviewModeImg2
+            // 
+            this.rbPreviewModeImg2.AutoSize = true;
+            this.rbPreviewModeImg2.Location = new System.Drawing.Point(13, 47);
+            this.rbPreviewModeImg2.Name = "rbPreviewModeImg2";
+            this.rbPreviewModeImg2.Size = new System.Drawing.Size(67, 19);
+            this.rbPreviewModeImg2.TabIndex = 1;
+            this.rbPreviewModeImg2.TabStop = true;
+            this.rbPreviewModeImg2.Text = "Image 2";
+            this.rbPreviewModeImg2.UseVisualStyleBackColor = true;
+            this.rbPreviewModeImg2.CheckedChanged += new System.EventHandler(this.rbPreviewModeImg_CheckedChanged);
+            // 
+            // rbPreviewModeImg1
+            // 
+            this.rbPreviewModeImg1.AutoSize = true;
+            this.rbPreviewModeImg1.Checked = true;
+            this.rbPreviewModeImg1.Location = new System.Drawing.Point(13, 22);
+            this.rbPreviewModeImg1.Name = "rbPreviewModeImg1";
+            this.rbPreviewModeImg1.Size = new System.Drawing.Size(67, 19);
+            this.rbPreviewModeImg1.TabIndex = 0;
+            this.rbPreviewModeImg1.TabStop = true;
+            this.rbPreviewModeImg1.Text = "Image 1";
+            this.rbPreviewModeImg1.UseVisualStyleBackColor = true;
+            this.rbPreviewModeImg1.CheckedChanged += new System.EventHandler(this.rbPreviewModeImg_CheckedChanged);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -83,6 +173,9 @@
             this.Text = "FrmMain";
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.gbPreviewMode.ResumeLayout(false);
+            this.gbPreviewMode.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -90,8 +183,14 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button btnLoadBitmap;
         private PictureBox pb;
         private OpenFileDialog od;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button btnLoadImage2;
+        private Button btnLoadImage1;
+        private GroupBox gbPreviewMode;
+        private RadioButton tbPreviewModeResult;
+        private RadioButton rbPreviewModeImg2;
+        private RadioButton rbPreviewModeImg1;
     }
 }
--- BitmapsPxDiff/FrmMain.cs
+++ BitmapsPxDiff/FrmMain.cs
@@ -2,19 +2,48 @@ namespace BitmapsPxDiff
 {
     public partial class FrmMain : Form
     {
-        private Bitmap bmp;
+        private enum ImagesIndexes { image1, image2, imageResult };
+        private int currentImageIndex = 0;
+        private Bitmap[] images = new Bitmap[3];
         public FrmMain()
         {
             InitializeComponent();
         }
 
-        private void btnLoadBitmap_Click(object sender, EventArgs e)
+        private void RefreshPreview()
         {
-            if (od.ShowDialog() != DialogResult.OK) return;
-            this.btnLoadBitmap.Text = Path.GetFileName(od.FileName);
-            bmp = new Bitmap(od.FileName);
-            pb.Image = bmp;
+            if (0 > currentImageIndex || currentImageIndex > images.Length) return;
+
+            pb.Image = images[currentImageIndex];
         }
 
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            if (!(sender is Button)) return;
+            Button btn = (Button)sender;
+            if ((0 > btn.TabIndex || btn.TabIndex > 1)
+                || (od.ShowDialog() != DialogResult.OK)) return;
+
+            btn.Text = Path.GetFileName(od.FileName);
+
+            images[btn.TabIndex] = new Bitmap(od.FileName);
+
+            if (btn.TabIndex == 0)
+                rbPreviewModeImg1.Checked = true;
+            else
+                rbPreviewModeImg2.Checked = true;
+            
+            RefreshPreview();
+        }
+
+        private void rbPreviewModeImg_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!(sender is RadioButton)) return;
+            RadioButton rb = (RadioButton)sender;
+            if ((!rb.Checked) 
+                || (0 > rb.TabIndex || rb.TabIndex > images.Length)) return;
+            currentImageIndex = rb.TabIndex;
+            RefreshPreview();
+        }
     }
 }
